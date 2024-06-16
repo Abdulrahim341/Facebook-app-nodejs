57b12e8bb48356286510121d3c308271e667188b
Abdulrahim341 <abdulrahimelaidy94@gmail.com>
2024-06-17 01:03:32 +0300

    init app

--- database/dbconection.js
+++ database/dbconection.js
@@ -1,8 +1,5 @@
 import {Sequelize,DataTypes}  from 'sequelize'
-const sequelize = new Sequelize('assignment6','root','',{
-    host:'localhost',
-    dialect:'mysql'
-})
+const sequelize = new Sequelize('mysql://uleq8pefbgvm0lr9:BW5BxvVRd8IZKr0h5ZPL@bzt4ny6bkdc7afn5feh6-mysql.services.clever-cloud.com:3306/bzt4ny6bkdc7afn5feh6')
 
 sequelize.authenticate().then(()=>{
 console.log('DB connected');
